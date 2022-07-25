ebcaf133fda839840e29045ac373f33c042cb256
Shahrear ahamed <its.shahrear@gmail.com>
2022-07-25 16:49:34 +0600

    basic counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const counterData = useSelector((state) => state.counter.value);
+
+  console.log(counterData);
+  const dispatch = useDispatch();
+
+  const update = () => {
+    dispatch(increment());
+  };
+  const minusUpdate = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My count {counterData}</h1>
+      <div>
+        <button onClick={update}>plus</button>
+        <button onClick={minusUpdate}>minus</button>
+      </div>
+      <div>
+        <h2>Post page are here</h2>
+      </div>
     </div>
   );
 }
